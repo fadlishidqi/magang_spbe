3027fee87e1a4ea0329205db35c8a520f0069c5d
Fadli Shidqi Firdaus <fadlishidqifirdaus@students.undip.ac.id>
2024-06-26 08:54:30 +0700

    tes unit

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/jon', function () {
-    return view('jon');
-}); 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
