e32ba187c99ddc344e8ae328bf888a3baa4764d4
riezquy <ahmadriezqi123@gmail.com>
2024-07-01 16:42:44 +0700

    faq bang

--- resources/views/faq/faq.blade.php
+++ resources/views/faq/faq.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Frequently Asked Questions</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
         integrity="sha512-PoQgnD+vMxwXQf2nx5nQZQ3Hg1K6qT+ly6wTFBo1hApKsyw4iP29XSq0O0jZt4QKlVDkB7w64+GuA0jQ/o8cZA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -19,20 +20,20 @@
             top: -400px;
             transition: top 0.s ease, opacity 0.5s ease;
         }
-        ul#navList.show {
-            opacity: 10;
+            ul#navList.show {
+                opacity: 10;
         }
 
-        .text-wrapper {
-            display: inline-block;
-            vertical-align: middle;
-            text-align: left;
-            font-size: 1rem; 
+            .text-wrapper {
+                display: inline-block;
+                vertical-align: middle;
+                text-align: left;
+                font-size: 1rem; 
         }
 
-        .text-wrapper span {
-            display: block;
-            line-height: 1.2; 
+            .text-wrapper span {
+                display: block;
+                line-height: 1.2; 
         }
 
         @media (max-width: 768px) {
@@ -166,104 +167,166 @@
 </script>
 
 <!-- FAQ SECTION -->
-<section class="mx-20">
-    <div class="max-w-4xl mx-auto py-10 flex flex-col md:flex-row justify-between space-y-8 md:space-y-0">
-        <div class="heading md:w-1/3">
-            <h1 class="text-4xl font-bold mb-8">Frequently asked questions</h1>
-            <div class="space-y-6">
-                <button class="flex items-center p-4 bg-white rounded-lg shadow" onclick="filterFAQ('Layanan')">
-                    <div class="flex-shrink-0 mr-4">
-                        <svg class="w-8 h-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l6.16-3.422A12.083 12.083 0 0118 12c0 4.418-3.582 8-8 8s-8-3.582-8-8c0-.379.034-.753.098-1.12L12 14z" />
-                        </svg>
-                    </div>
-                    <div>
-                        <h3 class="text-lg font-medium">Layanan</h3>
-                        <p class="text-gray-500"></p>
-                    </div>
-                </button>
-                <button class="flex items-center p-4 bg-white rounded-lg shadow" onclick="filterFAQ('Laporan')">
-                    <div class="flex-shrink-0 mr-4">
-                        <svg class="w-8 h-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16h6M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </div>
-                    <div>
-                        <h3 class="text-lg font-medium">Laporan</h3>
-                        <p class="text-gray-500"></p>
-                    </div>
-                </button>
-                <button class="flex items-center p-4 bg-white rounded-lg shadow" onclick="filterFAQ('Tracking')">
-                    <div class="flex-shrink-0 mr-4">
-                        <svg class="w-8 h-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2M12 7h3m0 0l3 3m-3-3l-3 3m-9 4h12m0 0l-3 3m3-3l3 3M3 19h12M9 17v2" />
-                        </svg>
-                    </div>
-                    <div>
-                        <h3 class="text-lg font-medium">Tracking</h3>
-                        <p class="text-gray-500"></p>
-                    </div>
-                </button>
-            </div>
+<section class="mx-4 sm:mx-10 lg:mx-20">
+    <div class="max-w-6xl mx-auto py-10">
+        <div class="heading text-center mb-8">
+            <h1 class="text-4xl font-bold mb-2">Frequently Asked Questions</h1>
+            <p class="mb-4">Layanan SPBE Pemerintah Kota Pekalongan</p>
         </div>
 
-        <div class="bg-white p-6 rounded-lg shadow md:w-2/3">
-            <div class="space-y-4">
-                <div class="faq-item" data-category="Layanan">
-                    <h2 class="text-2xl font-bold mb-4">Layanan</h2>
-                    @foreach ($faqs->where('kategori', 'Layanan') as $item)
-                        <div>
-                            <button
-                                class="w-full flex justify-between items-center text-left text-xl font-semibold py-2"
-                                onclick="toggleAnswer('{{ $item->id }}')">
-                                <span>{{ $item->pertanyaan }}</span>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
-                            <div id="{{ $item->id }}" class="hidden text-gray-600 max-w-fit">
-                                <p>{{ $item->jawaban }}</p>
-                            </div>
+        <div class="flex flex-col md:flex-row justify-between">
+            <!-- Kolom Kategori -->
+            <div class="w-full md:w-auto mb-6 md:mb-0 md:mr-8">
+                <div class="space-y-6">
+                    <button class="flex items-center p-4 bg-white rounded-lg shadow w-full" onclick="filterFAQ('Layanan')">
+                        <div class="flex-shrink-0 mr-4">
+                            <svg class="w-8 h-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l6.16-3.422A12.083 12.083 0 0118 12c0 4.418-3.582 8-8 8s-8-3.582-8-8c0-.379.034-.753.098-1.12L12 14z" />
+                            </svg>
                         </div>
-                    @endforeach
+                        <div>
+                            <h3 class="text-lg font-medium">Layanan</h3>
+                        </div>
+                    </button>
+                    <button class="flex items-center p-4 bg-white rounded-lg shadow w-full" onclick="filterFAQ('Laporan')">
+                        <div class="flex-shrink-0 mr-4">
+                            <svg class="w-8 h-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16h6M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </div>
+                        <div>
+                            <h3 class="text-lg font-medium">Laporan</h3>
+                        </div>
+                    </button>
+                    <button class="flex items-center p-4 bg-white rounded-lg shadow w-full" onclick="filterFAQ('Tracking')">
+                        <div class="flex-shrink-0 mr-4">
+                            <svg class="w-8 h-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2M12 7h3m0 0l3 3m-3-3l-3 3m-9 4h12m0 0l-3 3m3-3l3 3M3 19h12M9 17v2" />
+                            </svg>
+                        </div>
+                        <div>
+                            <h3 class="text-lg font-medium">Tracking</h3>
+                        </div>
+                    </button>
                 </div>
+            </div>
 
-                <div class="faq-item" data-category="Laporan">
-                    <h2 class="text-2xl font-bold mb-4">Laporan</h2>
-                    @foreach ($faqs->where('kategori', 'Laporan') as $item)
-                        <div>
-                            <button
-                                class="w-full flex justify-between items-center text-left text-xl font-semibold py-2"
-                                onclick="toggleAnswer('{{ $item->id }}')">
-                                <span>{{ $item->pertanyaan }}</span>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
-                            <div id="{{ $item->id }}" class="hidden text-gray-600 max-w-fit">
-                                <p>{{ $item->jawaban }}</p>
+            <!-- Kolom Pertanyaan dan Jawaban -->
+            <div class="flex-grow bg-white p-6 rounded-lg shadow">
+                <div class="space-y-4">
+                    <div class="faq-item" data-category="Layanan">
+                        <h2 class="text-2xl font-bold mb-4 mt-4">Layanan</h2>
+                        @foreach ($faqs->where('kategori', 'Layanan') as $item)
+                            <div>
+                                <button
+                                    class="w-full flex justify-between items-center text-left text-xl font-semibold py-2 transition-all duration-300"
+                                    onclick="toggleAnswer('{{ $item->id }}')">
+                                    <span>{{ $item->pertanyaan }}</span>
+                                    <i class="fas fa-chevron-down transform transition-transform duration-300"></i>
+                                </button>
+                                <div id="{{ $item->id }}" class="hidden text-gray-600">
+                                    <p>{{ $item->jawaban }}</p>
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
-                </div>
+                        @endforeach
+                    </div>
 
-                <div class="faq-item" data-category="Tracking">
-                    <h2 class="text-2xl font-bold mb-4">Tracking</h2>
-                    @foreach ($faqs->where('kategori', 'Tracking') as $item)
-                        <div>
-                            <button
-                                class="w-full flex justify-between items-center text-left text-xl font-semibold py-2"
-                                onclick="toggleAnswer('{{ $item->id }}')">
-                                <span>{{ $item->pertanyaan }}</span>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
-                            <div id="{{ $item->id }}" class="hidden text-gray-600 max-w-fit">
-                                <p>{{ $item->jawaban }}</p>
+                    <div class="faq-item" data-category="Laporan">
+                        <h2 class="text-2xl font-bold mb-4">Laporan</h2>
+                        @foreach ($faqs->where('kategori', 'Laporan') as $item)
+                            <div>
+                                <button
+                                    class="w-full flex justify-between items-center text-left text-xl font-semibold py-2 transition-all duration-300"
+                                    onclick="toggleAnswer('{{ $item->id }}')">
+                                    <span>{{ $item->pertanyaan }}</span>
+                                    <i class="fas fa-chevron-down transform transition-transform duration-300"></i>
+                                </button>
+                                <div id="{{ $item->id }}" class="hidden text-gray-600">
+                                    <p>{{ $item->jawaban }}</p>
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
+
+                    <div class="faq-item" data-category="Tracking">
+                        <h2 class="text-2xl font-bold mb-4">Tracking</h2>
+                        @foreach ($faqs->where('kategori', 'Tracking') as $item)
+                            <div>
+                                <button
+                                    class="w-full flex justify-between items-center text-left text-xl font-semibold py-2 transition-all duration-300"
+                                    onclick="toggleAnswer('{{ $item->id }}')">
+                                    <span>{{ $item->pertanyaan }}</span>
+                                    <i class="fas fa-chevron-down transform transition-transform duration-300"></i>
+                                </button>
+                                <div id="{{ $item->id }}" class="hidden text-gray-600">
+                                    <p>{{ $item->jawaban }}</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+    <script>
+        function toggleAnswer(id) {
+            var element = document.getElementById(id);
+            element.classList.toggle('hidden');
+            var icon = element.previousElementSibling.querySelector('i');
+            icon.classList.toggle('rotate-180');
+        }
+
+        function filterFAQ(category) {
+            var items = document.querySelectorAll('.faq-item');
+            items.forEach(item => {
+                if (item.getAttribute('data-category') === category || category === 'All') {
+                    item.classList.remove('hidden');
+                } else {
+                    item.classList.add('hidden');
+                }
+            });
+        }
+    </script>
+
+    <style>
+        .rotate-180 {
+            transform: rotate(180deg);
+        }
+
+        /* Media Queries */
+        @media (max-width: 768px) {
+            .faq-item h2 {
+                font-size: 1.5rem;
+            }
+
+            .faq-item button {
+                font-size: 1rem;
+            }
+        }
+
+        @media (max-width: 1024px) {
+            .faq-item h2 {
+                font-size: 1.75rem;
+            }
+
+            .faq-item button {
+                font-size: 1.125rem;
+            }
+        }
+
+        /* Styling the button section to be more responsive */
+        @media (max-width: 768px) {
+            .w-full.md\\:w-auto.mb-6.md\\:mb-0.md\\:mr-8 {
+                margin-right: 0 !important;
+            }
+        }
+    </style>
+
+
+
 
 <!-- FOOTER -->
 <footer class="bg-gray-100 p-8 text-left">
@@ -308,14 +371,12 @@
 @vite('resources/js/app.js')
 
 <script>
-    function toggleAnswer(id) {
-        var answer = document.getElementById(id);
-        if (answer.classList.contains('hidden')) {
-            answer.classList.remove('hidden');
-        } else {
-            answer.classList.add('hidden');
+function toggleAnswer(id) {
+            var element = document.getElementById(id);
+            element.classList.toggle('hidden');
+            var icon = element.previousElementSibling.querySelector('i');
+            icon.classList.toggle('rotate-180');
         }
-    }
 
     function filterFAQ(category) {
         // Sembunyikan semua FAQ
@@ -333,6 +394,7 @@
     document.addEventListener('DOMContentLoaded', function() {
         filterFAQ('Layanan'); // Atau ganti dengan kategori yang diinginkan secara default
     });
+    
 </script>
 
 </body>
